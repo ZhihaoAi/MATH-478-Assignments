900b24bc4c839f3717b0ebfaef71edeecd6d9db6
ZhihaoAi <zai@hawk.iit.edu>
2018-04-24 19:47:05 -0500

    Fix bug in hw5ca1

--- HW5/ca1.m
+++ HW5/ca1.m
@@ -5,8 +5,9 @@ for n = ns
 end
 
 function res = AB2(t0,y0,y1,f,N)
-	h = 1/N; ys(1) = y0; ys(2) = y1;
+	h = 1/N;
 	ys = zeros(1,N+1); ts = linspace(t0,t0+N*h,N+1);
+	ys(1) = y0; ys(2) = y1;
 	for n=1:N-1
 	   f1 = f(ts(n),ys(n)); f2 = f(ts(n+1),ys(n+1));
 	   ys(n+2) = ys(n+1) + h/2*(3*f2-f1);
@@ -15,8 +16,9 @@ function res = AB2(t0,y0,y1,f,N)
 end
 
 function res = BDF2(t0,y0,y1,N)
-	h = 1/N; ys(1) = y0; ys(2) = y1;
+	h = 1/N;
 	ys = zeros(1,N+1); ts = linspace(t0,t0+N*h,N+1);
+	ys(1) = y0; ys(2) = y1;
 	for n=1:N-1
 	   ys(n+2) = (-ys(n)+4*ys(n+1)+200*h*cos(ts(n+2))-2*h*sin(ts(n+2)))/(3+200*h);
 	end
